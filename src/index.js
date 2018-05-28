523409f19a52dba17671523fbfc9a0027b0c8a2c
lisfer <makrushinds@gmail.com>
2018-05-28 15:58:29 +0300

    SeaField with empty cells

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+
+class Cell extends React.Component {
+    render () {
+        return (
+            <div className="cell">&nbsp;</div>
+        )
+    }
+}
+
+class SeaField extends React.Component {
+    render () {
+        let self = this;
+        let rows = [];
+
+        for (let i = 0; i < self.props.max_y; i++) {
+            let cells = [];
+            for (let j = 0; j < self.props.max_x; j++) {
+                cells[j] = <Cell x={j} y={i}/>
+            }
+            rows[i] = <div>{cells}</div>;
+        }
+
+        return (
+            <div>
+                {rows}
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(
+  <SeaField max_x={10} max_y={10}/>,
+  document.getElementById('root')
+);

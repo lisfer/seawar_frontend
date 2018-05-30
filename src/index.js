87c11a7fe854dd00b5934589dbba5b0a3cd08c57
lisfer <makrushinds@gmail.com>
2018-05-30 08:37:34 +0300

    user shoot

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-const SEA = {EMPTY:0, SHIP:10, BORDER:1};
+const SEA = {EMPTY:0, SHIP:10, BORDER:1, MISSED: -1, HIT: -10};
+const SIGNALS = {HIT:2, MISSED:3, KILLED:5, WIN:5};
+const SERVER = 'http://localhost:5000';
 
 class Cell extends React.Component {
 
     render () {
 
         let className = 'cell';
-        if (this.props.value === SEA.SHIP) className += ' shipCellUser';
-        if (this.props.value === SEA.BORDER) className += ' borderCell';
+        if (this.props.value === SEA.SHIP) className += ' cellShipUser';
+        if (this.props.value === SEA.BORDER) className += ' cellBorder';
+        if (this.props.value === SEA.MISSED) className += ' cellMissed';
+        if (this.props.value === SEA.HIT) className += ' cellHit';
         return (
-            <div className={className}>&nbsp;</div>
+            <div className={className} onClick={this.props.clickHandler}>&nbsp;</div>
         )
     }
 }
@@ -45,16 +49,24 @@ class SeaField extends React.Component {
             max_y: this.props.max_y,
             cells: this.createMatrix(this.props.max_x, this.props.max_y)
         };
+        this.initShips();
+    }
+
+    initShips () {}
+
+    renderCells () {
+
+        return this.state.cells.map(
+            (row, y) => (
+                <div key={y}>
+                    {row.map(
+                        (el, x) => <Cell key={`${y}_${x}`} x={x} y={y} value={el.value}/>)}
+                </div>));
     }
 
     render () {
-        let self = this;
 
-        let cells = self.state.cells.map(
-            (row, y) =>  (
-                <div key={y}>
-                    {row.map((el, x) => { return <Cell key={`${y}_${x}`} x={x} y={y} value={el.value}/>})}
-                </div>));
+        let cells = this.renderCells();
 
         return (
             <div className ="seaField">{cells}</div>
@@ -65,19 +77,57 @@ class SeaField extends React.Component {
 
 class SeaFieldUser extends SeaField {
 
+    initShips() {
+        let self = this;
+        fetch(SERVER + '/api/init_user_ship', {method: 'POST', credentials: 'include'})
+            .then(
+                (resp) => {if (!resp.ok) throw Error(resp.statusText); return resp.json()})
+            .then(
+                (data) => self.set_many(data.filter((el) => el.value === SEA.SHIP)))
+            .catch((err) => console.log(err));
+    }
+}
+
+
+class SeaFieldComp extends SeaField {
+
     constructor(props) {
         super(props);
-        this.initShips();
+        this.state['frozen'] = false;
     }
 
     initShips() {
+        fetch(SERVER + '/api/init_enemy_ship', {method: 'POST', credentials: 'include'})
+            .then((resp) => {if (!resp.ok) throw Error(resp.statusText); return resp.json()})
+            .catch((err) => console.log(err));
+    }
+
+    cellClick(x, y) {
         let self = this;
-        fetch('http://localhost:5000/init_user_ship', {method: 'POST'})
-            .then(
-                (response) => response.json())
-            .then(
-                (data) => self.set_many(data.filter((el) => el.value === SEA.SHIP)))
-            .catch(function(error) { console.log(error); });
+        if (self.state.frozen) return;
+        let data = new FormData();
+        data.append('x', x);
+        data.append('y', y);
+        fetch(SERVER + '/api/user_shoot', {method: 'POST', body: data, credentials: 'include'})
+            .then((resp) => {if (!resp.ok) throw Error(resp.statusText); return resp.json()})
+            .then((data) => {
+                self.set_many(data.cells.map((el) => {return el}));
+                self.set_many(data.border.map((el) => {el.value = el.value === SEA.EMPTY ? SEA.BORDER : el.value; return el}));
+            })
+            .catch((err) => console.log(err))
+            .finally(() => self.setState({frozen: false}));
+
+        self.setState({frozen: true});
+    }
+
+    renderCells () {
+        return this.state.cells.map(
+            (row, y) => (
+                <div key={y}>
+                    {row.map(
+                        (el, x) => <Cell key={`${y}_${x}`} clickHandler={() => this.cellClick(x, y)}
+                                         x={x} y={y} value={el.value}/>)}
+                </div>));
     }
 }
 
@@ -85,7 +135,7 @@ class SeaFieldUser extends SeaField {
 ReactDOM.render(
     <div>
         <SeaFieldUser max_x={10} max_y={10}/>
-        <SeaField max_x={10} max_y={10}/>
+        <SeaFieldComp max_x={10} max_y={10}/>
     </div>,
   document.getElementById('root')
 );

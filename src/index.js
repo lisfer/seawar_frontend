92d7a02b711209c0839c715ebd68866ea5b7771f
lisfer <makrushinds@gmail.com>
2018-06-01 11:26:40 +0300

    updated field inits

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-const SEA = {EMPTY:0, SHIP:10, BORDER:1, MISSED: -1, HIT: -10};
+const SEA = {EMPTY: 'empty', SHIP: 'ship', BORDER: 'border', MISSED: -1, HIT: -10};
 const SIGNALS = {HIT:2, MISSED:3, KILLED:5, WIN:5};
 const SERVER = 'http://localhost:5000';
 
@@ -24,43 +24,59 @@ class Cell extends React.Component {
 
 class SeaField extends React.Component {
 
-    createMatrix(max_x, max_y) {
-        /*
-        creates Array [max_y][max_y]
-        where every element is a Object{x, y, is_ship, is_border} keys
-         */
-        return Array.from(
-                {length: max_y},
-                (v, i) => Array.from(
-                    {length: max_x},
-                    (v, j) => ({x: j, y: i, value: SEA.EMPTY})));
+    constructor(props) {
+        super(props);
+        this.state = {
+            cells: [],
+            maxX: null,
+            maxY: null
+        }
+        this.initField();
     }
 
-    set_many(cells_update) {
+    initField() {}
+
+    createMatrix(maxX, maxY) {
+        /*
+        creates Array [maxY][maxY]
+        where every element is a Object{x, y, is_ship, is_border} keys
+         */
+        return Array.from({length: maxY}, (v, i) => Array.from(
+            {length: maxX},
+            (v, j) => ({x: j, y: i, value: SEA.EMPTY})));
+    }
+
+    updateCoordinated(coords, value) {
+        /*
+            @param coords: list of two-values lists: [ [x, y], ...]
+            @param value:
+         */
+        let stateCells = this.state.cells;
+        coords.map((el) => {
+            stateCells[el[1]][el[0]].value = value;
+        })
+        this.setState({cells: state_cells});
+    }
+
+    setMany(cells_update, value) {
         let state_cells = this.state.cells;
         cells_update.map((el) => (state_cells[el.y][el.x].value = el.value));
         this.setState({cells: state_cells});
     }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            max_x: this.props.max_x,
-            max_y: this.props.max_y,
-            cells: this.createMatrix(this.props.max_x, this.props.max_y)
-        };
-        this.initShips();
+    createField (maxX, maxY) {
+        this.setState({
+            maxX: maxX,
+            maxY: maxY,
+            cells: this.createMatrix(maxX, maxY)
+        })
     }
 
-    initShips () {}
-
     renderCells () {
-
         return this.state.cells.map(
             (row, y) => (
                 <div key={y}>
-                    {row.map(
-                        (el, x) => <Cell key={`${y}_${x}`} x={x} y={y} value={el.value}/>)}
+                    {row.map((el, x) => <Cell key={`${y}_${x}`} x={x} y={y} value={el.value}/>)}
                 </div>));
     }
 
@@ -77,13 +93,16 @@ class SeaField extends React.Component {
 
 class SeaFieldUser extends SeaField {
 
-    initShips() {
+    initField() {
         let self = this;
         fetch(SERVER + '/api/init_user_ship', {method: 'POST', credentials: 'include'})
             .then(
                 (resp) => {if (!resp.ok) throw Error(resp.statusText); return resp.json()})
             .then(
-                (data) => self.set_many(data.filter((el) => el.value === SEA.SHIP)))
+                (data) => {
+                    self.createField(data.max_x, data.max_y);
+                    self.updateCoordinated(data.ships, SEA.SHIP);
+                })
             .catch((err) => console.log(err));
     }
 }
@@ -96,9 +115,12 @@ class SeaFieldComp extends SeaField {
         this.state['frozen'] = false;
     }
 
-    initShips() {
+    initField() {
         fetch(SERVER + '/api/init_enemy_ship', {method: 'POST', credentials: 'include'})
-            .then((resp) => {if (!resp.ok) throw Error(resp.statusText); return resp.json()})
+            .then(
+                (resp) => {if (!resp.ok) throw Error(resp.statusText); return resp.json()})
+            .then(
+                (data) => this.createField(data.max_x, data.max_y))
             .catch((err) => console.log(err));
     }
 
@@ -109,11 +131,21 @@ class SeaFieldComp extends SeaField {
         data.append('x', x);
         data.append('y', y);
         fetch(SERVER + '/api/user_shoot', {method: 'POST', body: data, credentials: 'include'})
-            .then((resp) => {if (!resp.ok) throw Error(resp.statusText); return resp.json()})
-            .then((data) => {
-                self.set_many(data.cells.map((el) => {return el}));
-                self.set_many(data.border.map((el) => {el.value = el.value === SEA.EMPTY ? SEA.BORDER : el.value; return el}));
-            })
+            .then(
+                (resp) => {
+                    if (!resp.ok) throw Error(resp.statusText);
+                    return resp.json()
+                })
+            .then(
+                (data) => {
+                    self.setMany(data.cells.map((el) => {
+                        return el
+                    }));
+                    self.setMany(data.border.map((el) => {
+                        el.value = el.value === SEA.EMPTY ? SEA.BORDER : el.value;
+                        return el
+                    }));
+                })
             .catch((err) => console.log(err))
             .finally(() => self.setState({frozen: false}));
 
